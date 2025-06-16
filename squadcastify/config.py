c9a6627dc5e7427ab91928f821a38828c72eb54f
Mohammad Muazam <mohammad.muazam@solarwinds.com>
2025-06-16 14:39:01 +0530

    fix: pass settings as arg

--- squadcastify/config.py
+++ squadcastify/config.py
@@ -6,13 +6,10 @@ class Settings(BaseSettings):
     source: str = os.getenv("SOURCE", "opsgenie")
 
     state_dir: str = os.getenv("STATE_DIR", "terraform_state")
-
     pagerduty_api_token: str = os.getenv("PAGERDUTY_API_TOKEN", "")
     pagerduty_api_url: str = os.getenv("PAGERDUTY_API_URL", "https://api.pagerduty.com")
-
     opsgenie_api_key: str = os.getenv("OPSGENIE_API_KEY", "")
     opsgenie_api_url: str = os.getenv("OPSGENIE_API_URL", "https://api.opsgenie.com/v2")
-
     squadcast_refresh_token: str = os.getenv("SQUADCAST_REFRESH_TOKEN", "")
     squadcast_region: str = os.getenv("SQUADCAST_REGION", "us")
 
